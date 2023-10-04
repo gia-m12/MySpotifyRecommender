f8968c36400267d17952c4cdce1603c79c6f616c
gia-m12 <73036167+gia-m12@users.noreply.github.com>
2023-10-04 07:56:02 -0400

    Create apiSetup.py

---
+++ apiSetup.py
@@ -0,0 +1,12 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="YOUR_APP_CLIENT_ID",
+                                               client_secret="YOUR_APP_CLIENT_SECRET",
+                                               redirect_uri="YOUR_APP_REDIRECT_URI",
+                                               scope="user-library-read"))
+
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
