f8a46c07f8f2183bf11235dc103310516fed1cc0
gia-m12 <tul55523@temple.edu>
2023-10-04 08:13:58 -0400

    Setting up Spotify API with client ID, client secret, and redirect URL that was given through spotify

--- apiSetup.py
+++ apiSetup.py
@@ -1,9 +1,9 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="YOUR_APP_CLIENT_ID",
-                                               client_secret="YOUR_APP_CLIENT_SECRET",
-                                               redirect_uri="YOUR_APP_REDIRECT_URI",
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="4f57bdd6990d43bba5b4b45c4d7c1589",
+                                               client_secret="45749f9145934ef39fe3c69e9e7fdc63",
+                                               redirect_uri="http://localhost:8888/callback",
                                                scope="user-library-read"))
 
 results = sp.current_user_saved_tracks()
